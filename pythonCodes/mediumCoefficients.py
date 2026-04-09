c9bee71f42a638dfd38f2307d129f1baed4ea08f
liukushka <liukushka@liukushka.homenet.telecomitalia.it>
2026-04-09 11:29:13 +0200

    .

--- pythonCodes/mediumCoefficients.py
+++ pythonCodes/mediumCoefficients.py
@@ -1,12 +1,11 @@
 from numpy import *
 import pandas as pd
+
 df = pd.read_csv(
-    'forceCoeffs/2220/forceCoeffs.dat', sep = "\s+", skiprows = 9,
+    'forceCoeffs/0/forceCoeffs.dat', sep = "\s+", skiprows = 9,
     names = ['Time', 'CL', 'CD', 'CM', 'CL (f)', 'CL (r)']
 )
 
-a = int(input("Enter the number of last iterations to average with: "))
-
-meanCD = df.loc[-a:, 'CD'].mean()
+meanCD = df.loc[-1000:, 'CD'].mean()
 
 print(f"The mean drag coefficient is: {meanCD}")
